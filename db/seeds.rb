71196defc94e1b8fb2d26c2d36e8fce9fbebdcb8
Eli Duke <elimduke@gmail.com>
2018-07-06 11:01:04 -0700

    Builds out seeds with song list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# CREATE SONGS
+[
+  "Hell No",
+  "Hot Won't Quit",
+  "Break New Ground",
+  "I Know",
+  "Bound to Drown",
+  "Stoned to the Bone",
+  "Hate Me So Much / In the End",
+  "Infamy",
+  "Velvet Fog",
+  "Gettin' Hard",
+  "Crib Hop / Underwater",
+  "Hiccups",
+  "Grab the Wheel",
+  "Quality Education",
+  "One Two Three ONE TWO",
+  "Get What For",
+  "This Hole I've Dug for Myself",
+  "Night Owls"
+].each do |name|
+  Song.create!(name: name)
+end
