6daff56dbcf36e28d66eb7f5a82714664d914b44
Eli Duke <elimduke@gmail.com>
2018-07-06 10:20:56 -0700

    scaffold Songs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_06_171003) do
+ActiveRecord::Schema.define(version: 2018_07_06_171927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2018_07_06_171003) do
   create_table "setlists", force: :cascade do |t|
     t.string "name"
     t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
