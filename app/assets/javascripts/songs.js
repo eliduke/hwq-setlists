9c3768659c454b50a6b61c285f6158b133bb9088
Eli Duke <elimduke@gmail.com>
2018-07-17 00:32:14 -0700

    Move song checkboxes around as you check and uncheck

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,3 +1,22 @@
 $(function() {
   $('.sortable').sortable();
+
+  $("input[type='checkbox']").change(function() {
+    if(this.checked) {
+      var checkedBoxes = $("input[type='checkbox']:checked")
+      if (checkedBoxes.length > 1) {
+        var lastCheckedBox = checkedBoxes[checkedBoxes.length - 2].parentElement.parentElement;
+        $(this.parentElement.parentElement).insertAfter(lastCheckedBox);
+      } else {
+        var uncheckedBoxes = $("input[type='checkbox']:not(:checked)")
+        var firstUncheckedBox = uncheckedBoxes[0].parentElement.parentElement;
+        $(this.parentElement.parentElement).insertBefore(firstUncheckedBox);
+      }
+    } else {
+      var uncheckedBoxes = $("input[type='checkbox']:not(:checked)")
+      var firstUncheckedBox = uncheckedBoxes[1].parentElement.parentElement;
+      $(this.parentElement.parentElement).insertBefore(firstUncheckedBox);
+    }
+  });
 });
+
